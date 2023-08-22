78ab05c7f4cc7cabd5ba7afded45e1d669483220
Jon Mulieri <jonathan.mulieri@gmail.com>
2023-08-22 00:29:45 -0700

    Update attributes for orders and add leg class

--- lib/tradier/order.rb
+++ lib/tradier/order.rb
@@ -5,10 +5,29 @@ module Tradier
     attr_reader :id, :order_id, :type, :price, :symbol, :side, :quantity, \
     :status, :time_in_force, :option_type, :exec_inst, :exec_status, :extended_hours, \
     :trailing_limit_type, :trailing_stop_type, :request_date, :response_date, :num_legs, \
-    :errors, :result, :stop_price
+    :errors, :result, :stop_price, :duration, :avg_fill_price, :exec_quantity, :last_fill_price, \
+    :remaining_quantity, :create_date, :transaction_date, :strategy
+
+    class Leg < Tradier::Base
+      attr_reader :id, :type, :symbol, :side, :quantity, :status, :duration, :price, \
+      :avg_fill_price, :exec_quantity, :last_fill_price, :last_fill_quantity, :remaining_quantity, \
+      :create_date, :transaction_date, :option_symbol
+
+      def leg_class
+        attrs[:class]
+      end
+    end
 
     def self.from_response(body={})
       new(body[:order] || body)
+    end
+
+    def order_class
+      attrs[:class]
+    end
+
+    def legs
+      attrs[:leg]&.map { |l| Leg.new(l) }
     end
   end
 end
